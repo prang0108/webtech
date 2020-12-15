923d20b460485ef8e54fb966a711998cf76dbdc1
prang0108 <76036863+prang0108@users.noreply.github.com>
2020-12-15 16:17:21 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -12,8 +12,6 @@
 <p>ชื่อหลักสูตรภาษาไทย : หลักสูตรวิทยาศาสตรบัณฑิต สาขาวิชาวิทยาการข้อมูล</p>
 <p>ภาษาอังกฤษ : Bachelor of Science Program in Data Science</p>
 
-<img src="https://logos-world.net/wp-content/uploads/2020/04/YouTube-Logo-2017%E2%80%93present.jpg" width="500" height="300"
-
 <a href="https://www.cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">รายละเอียดหลักสูตร วิทยาการข้อมูล
 <br>
 <img src="https://logos-world.net/wp-content/uploads/2020/04/YouTube-Logo-2017%E2%80%93present.jpg" width="500" height="300"</a>
