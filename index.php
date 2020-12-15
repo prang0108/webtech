01a813d912e39f144b502d97524d4f8a6d03519d
prang0108 <76036863+prang0108@users.noreply.github.com>
2020-12-15 15:38:48 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="th">
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>ทดสอบ HTML</title>
+  </head>
+  <body>
+        <h1>สวัสดี ศิลปากร</h1>
+  </body>
+</html>
